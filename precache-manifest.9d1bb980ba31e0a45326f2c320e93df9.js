bbc0115a6ab451b2922f186dc211a8695cab73be
eslamicoo912 <eslamicoo3@gmail.com>
2022-06-13 11:46:43 +0200

    Updates

---
+++ precache-manifest.9d1bb980ba31e0a45326f2c320e93df9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1770f8f368e1c6d33782",
+    "url": "/My-Portfolio/static/css/main.e718e58c.chunk.css"
+  },
+  {
+    "revision": "1770f8f368e1c6d33782",
+    "url": "/My-Portfolio/static/js/main.1770f8f3.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/My-Portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "aa9d0237064e9f092376",
+    "url": "/My-Portfolio/static/js/runtime~main.aa9d0237.js"
+  },
+  {
+    "revision": "365832939920c9022d07ccc252560473",
+    "url": "/My-Portfolio/index.html"
+  }
+];
